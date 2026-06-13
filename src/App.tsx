1bc3ae358b4908d65ac014a987b45340632601cd
Binosko <abab86620@gmail.com>
2026-06-14 00:48:00 +0100

    Add project files

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Home from './Home';
 import Admin from './Admin';
 import NotFound from './NotFound';
 
+const ADMIN_HASH = '#admin_x7K9mP2vL4nQ8wR3tY6hB1jF5cD0zA9sE3gU7iO2pM8kX4qW6yV1eR5nT9';
+
 export default function App() {
   const [currentPath, setCurrentPath] = useState(window.location.pathname);
   const [currentHash, setCurrentHash] = useState(window.location.hash);
@@ -26,7 +28,7 @@ export default function App() {
     return <NotFound />;
   }
 
-  if (currentHash === '#admin') {
+  if (currentHash === ADMIN_HASH) {
     return <Admin />;
   }
 
